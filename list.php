22a57c6651b1fae37b66ee4a293652d36625a762
fnooub <61742373+fnooub@users.noreply.github.com>
2020-08-15 19:14:30 +0700

    update

--- list.php
+++ list.php
@@ -3,7 +3,7 @@
 include 'functions.php';
 
 $id = $_GET['id'] ?? 10577;
-$base_url = 'http://truyensac.herokuapp.com/';
+$base_url = 'http://localhost/curl_sinodancc/';
 
 $single_curl = single_curl('https://dichngay.com/translate?u=https://m.sinodan.cc/list/' . $id . '.html');
 
@@ -22,6 +22,8 @@ preg_match_all('@<ul class="list">(.*?)</ul>@si', $single_curl, $list_urls);
 //print_r($list_urls[1][1]);
 
 $trang_tiep = isset($_GET['nextpg']) ? $list_urls[0][0] : $list_urls[1][1];
+$nextpg = isset($_GET['nextpg']) ? '&nextpg' : null;
+
 
 preg_match_all('@<a .+?(\d+).html.+?>\s*(.+?)\s*</a>@si', $trang_tiep, $links);
 
@@ -70,17 +72,39 @@ if (isset($_GET['download'])) {
 	exit('ok');
 }
 
+if (isset($_GET['list_download']) && $id) {
+	echo '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">';
+	if (count($links[1]) > 10) {
+		$count = ceil(count($links[1]) / 10);
+	} else {
+		$count = 1;
+	}
+	if ($count > 1) {
+		$tong = count($links[1]);
+		for ($i = 1; $i <= $count; $i++) { 
+			$start = ($i - 1) * 10 + 1; //10 la so khoang cach giua bd va kt
+			$bd = ($i == 1) ? $start - 1 : $start;
+			$kt = ($i == 3) ? $tong : $start + 9;
 
-echo '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">';
+			echo '<p><a href="list.php?id=' . $id . $nextpg . '&download&s=' . $bd . '&e=' . $kt . '">' . $bd . ' - ' . $kt . '</a></p>';
+		}
+	} else {
+		echo "<p>Ít chương tải ngoài trang chủ</p>";
+	}
+	exit;
+}
+
+echo '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>a { text-decoration: none }</style>';
 echo '<h1>' . $tieude[1] . '</h1>';
 echo '<p>' . $mota[1] . '</p>';
-echo '<a href="list.php?id=' . $id . '&download">Tai ve TXT</a>';
-echo ' | <a href="list.php?id=' . $id . '&view">Xem HTML</a>';
-echo ' | <a href="list.php?id=' . $id . '&view&txt">Xem TXT</a>';
+echo '<a href="list.php?id=' . $id . $nextpg . '&download">Tai ve TXT</a>';
+echo ' | <a href="list.php?id=' . $id . $nextpg . '&list_download">Tai ve list</a>';
+echo ' | <a href="list.php?id=' . $id . $nextpg . '&view">Xem HTML</a>';
+echo ' | <a href="list.php?id=' . $id . $nextpg . '&view&txt">Xem TXT</a>';
 echo '<hr>';
 
 for ($i=0; $i < count($links[1]); $i++) { 
-	echo '<p><a href="view.php?id=' . $links[1][$i] . '">' . $links[2][$i] . '</a></p>';
+	echo '<p>[' . $i .'] <a href="view.php?id=' . $links[1][$i] . '">' . $links[2][$i] . '</a></p>';
 }
 
 if (isset($nextpage[1])) {
