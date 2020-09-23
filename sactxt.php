7d883815d842e7d051429eded0295253f1f85fae
fnooub <61742373+fnooub@users.noreply.github.com>
2020-09-23 23:05:08 +0700

    Add files via upload

--- sactxt.php
+++ sactxt.php
@@ -11,11 +11,14 @@ $data = json_decode(file_get_contents('sactxt_data.json'));
 <body class="w3-light-gray">
 
 <div class="w3-container">
+
+	<h2><?= count($data) ?> truyện</h2>
 	
 	<?php foreach ($data as $row): ?>
 		
 		<div class="w3-section w3-border w3-round w3-padding w3-white">
 			<h3 class="w3-medium"><a href="https://docs.google.com/uc?id=<?= $row->drive_id ?>"><?= $row->tieude ?></a></h3>
+			<div class="w3-tag w3-small"><?= myfilesize($row->size) ?></div>
 			<p class="w3-small w3-text-gray"><?= $row->mota ?></p>
 		</div>
 
@@ -25,3 +28,16 @@ $data = json_decode(file_get_contents('sactxt_data.json'));
 
 </body>
 </html>
+
+<?php
+
+function myfilesize($size, $precision = 2) {
+	static $units = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
+	$step = 1024;
+	$i = 0;
+	while (($size / $step) > 0.9) {
+		$size = $size / $step;
+		$i++;
+	}
+	return round($size, $precision).$units[$i];
+}
