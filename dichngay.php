52894e5d4cde021677445557d96abb9ad36d17f5
fnooub <61742373+fnooub@users.noreply.github.com>
2021-04-21 22:35:54 +0700

    Update dichngay.php

--- dichngay.php
+++ dichngay.php
@@ -1,8 +1,6 @@
 <?php
-
-if (isset($_POST['submit'])) {
 	include "functions.php";
-	$file = single_curl('https://dichngay.com/translate?u=' . $_POST['link']);
+	$file = single_curl('https://dichngay.com/translate?u=' . $_GET['link']);
 
 	//$file = nl2br($file);
 
@@ -10,20 +8,10 @@ if (isset($_POST['submit'])) {
 
 	header("Content-Type: text/plain");
 
-	if (isset($_POST['line'])) {
+	if (isset($_GET['line'])) {
 		$nd = preg_replace('/\n+/', ' ', $out[1]);
 		$nd = str_replace(". ", ".\n\n", $nd);
 		echo $nd;
 	} else {
 		echo $out[1];
 	}
-	exit;
-}
-?>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<form action="" method="post">
-	<input type="text" name="link"><br>
-	<input type="radio" id="line" name="line" value="line">
-	<label for="line">line</label><br>
-	<input type="submit" name="submit">
-</form>
